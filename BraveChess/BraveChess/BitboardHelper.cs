03813bb447c6a165d28414f15a451f86db0282b1
TalonTwist <s00118944@mail.itsligo.ie>
2014-02-18 00:16:50 +0000

    Added GetSetBits method(returns array storing the set bits from a Uint64) in Bitboard.cs

--- BraveChess/BraveChess/BitboardHelper.cs
+++ BraveChess/BraveChess/BitboardHelper.cs
@@ -55,9 +55,47 @@ namespace BraveChess
             0x332a4081140200L, 0x10301802830400L,  0x88240002008080L, 0x8018028040080L, 0x20030010060a900L, 0x19220045508200L, 0x40002010004001c0L,0x102042111804200L,
             0x1000200608243L,  0xc0900220024a401L,  0x801000804000603L,0x8015001002441801L, 0x211009001200509L,  0x4082001007241L,  0x1008010400021L,0x8080010a601241L};
 
+
+        //needs to be rearranged index[0] = h1
+        public static ulong[] magicNumberBishop =new ulong[] {
+        0x2910054208004104L, 0x2100630a7020180L, 0x5822022042000000L, 0x2ca804a100200020L, 0x204042200000900L, 0x2002121024000002L, 0x80404104202000e8L, 0x812a020205010840L, 0x8005181184080048L, 0x1001c20208010101L, 0x1001080204002100L, 0x1810080489021800L, 0x62040420010a00L, 0x5028043004300020L, 0xc0080a4402605002L, 0x8a00a0104220200L, 0x940000410821212L, 0x1808024a280210L, 0x40c0422080a0598L, 0x4228020082004050L, 0x200800400e00100L, 0x20b001230021040L, 0x90a0201900c00L, 0x4940120a0a0108L, 0x20208050a42180L, 0x1004804b280200L, 0x2048020024040010L, 0x102c04004010200L, 0x20408204c002010L, 0x2411100020080c1L, 0x102a008084042100L, 0x941030000a09846L, 0x244100800400200L, 0x4000901010080696L, 0x280404180020L, 0x800042008240100L, 0x220008400088020L, 0x4020182000904c9L, 0x23010400020600L, 0x41040020110302L, 0x412101004020818L, 0x8022080a09404208L, 0x1401210240484800L, 0x22244208010080L, 0x1105040104000210L, 0x2040088800c40081L, 0x8184810252000400L, 0x4004610041002200L, 0x40201a444400810L, 0x4611010802020008L, 0x80000b0401040402L, 0x20004821880a00L, 0x8200002022440100L, 0x9431801010068L, 0x1040c20806108040L, 0x804901403022a40L, 0x2400202602104000L, 0x208520209440204L, 0x40c000022013020L, 0x2000104000420600L, 0x400000260142410L, 0x800633408100500L, 0x2404080a1410L, 0x138200122002900L    
+    };
+        //same here
+        public static ulong[] occupancyMaskBishop = new ulong[] {
+        0x40201008040200L, 0x402010080400L, 0x4020100a00L, 0x40221400L, 0x2442800L, 0x204085000L, 0x20408102000L, 0x2040810204000L, 0x20100804020000L, 0x40201008040000L, 0x4020100a0000L, 0x4022140000L, 0x244280000L, 0x20408500000L, 0x2040810200000L, 0x4081020400000L, 0x10080402000200L, 0x20100804000400L, 0x4020100a000a00L, 0x402214001400L, 0x24428002800L, 0x2040850005000L, 0x4081020002000L, 0x8102040004000L, 0x8040200020400L, 0x10080400040800L, 0x20100a000a1000L, 0x40221400142200L, 0x2442800284400L, 0x4085000500800L, 0x8102000201000L, 0x10204000402000L, 0x4020002040800L, 0x8040004081000L, 0x100a000a102000L, 0x22140014224000L, 0x44280028440200L, 0x8500050080400L, 0x10200020100800L, 0x20400040201000L, 0x2000204081000L, 0x4000408102000L, 0xa000a10204000L, 0x14001422400000L, 0x28002844020000L, 0x50005008040200L, 0x20002010080400L, 0x40004020100800L, 0x20408102000L, 0x40810204000L, 0xa1020400000L, 0x142240000000L, 0x284402000000L, 0x500804020000L, 0x201008040200L, 0x402010080400L, 0x2040810204000L, 0x4081020400000L, 0xa102040000000L, 0x14224000000000L, 0x28440200000000L, 0x50080402000000L, 0x20100804020000L, 0x40201008040200L     
+    };
+        //and here
+        public static int[] magicNumberShiftsBishop = new int[] {
+    58,59,59,59,59,59,59,58,59,59,59,59,59,59,59,59,
+    59,59,57,57,57,57,59,59,59,59,57,55,55,57,59,59,
+    59,59,57,55,55,57,59,59,59,59,57,57,57,57,59,59,
+    59,59,59,59,59,59,59,59,58,59,59,59,59,59,59,58
+};
+
+        public static ulong[][] occupancyVariation;
+        public static ulong[][] occupancyAttackSet;
+
+
+        public static ulong[][] magicMovesRook;
+        public static ulong[][] magicMovesBishop;
+
 #endregion
 
         #region public methods
+
+        public BitboardHelper()
+        {
+            occupancyAttackSet = new ulong[64][];
+            occupancyVariation = new ulong[64][];
+
+            magicMovesRook = new ulong[64][];
+            generateOccupancyVariations(true);
+            generateMoveDatabase(true);
+
+            magicMovesBishop = new ulong[64][];
+            generateOccupancyVariations(false);
+            generateMoveDatabase(false);
+        }
 
         static public int getIndexFromSquare(Square s)
         {
@@ -129,6 +167,131 @@ namespace BraveChess
             }
             return null;
         }
+
+        //static public int GetStatus(string target, ulong occupiedBB, Square s)
+        //{
+        //    ulong result = 0;
+        //    switch(target)
+        //    {
+        //        case "rank":
+        //            result = occupiedBB >> (8 * (int)s.rank);
+        //            break;
+        //        case "file":
+        //            result = occupiedBB >> (8 * (int)s.file);
+        //            break;
+        //    }
+
+        //    return (int)result;
+        //}
+
+        int CountSetBits(UInt64 x) //http://en.wikipedia.org/wiki/Hamming_weight
+        {
+            int count;
+            for (count = 0; x != 0; count++)
+                x &= x - 1;
+            return count;
+        }
+
+         public void generateOccupancyVariations(bool isRook) //unfinished
+    {
+        int i, j, bitRef;
+        ulong mask;
+        int variationCount;
+        int[] setBitsInMask, setBitsInIndex;
+        int[] bitCount = new int[64];
+        
+        for (bitRef=0; bitRef<=63; bitRef++)
+        {
+            mask = isRook ? occupancyMaskRook[bitRef] : occupancyMaskBishop[bitRef];
+            setBitsInMask = Bitboards.getSetBits(mask); //----------------------------> NEED TO WRITE GetSetBits() method <----------------
+            bitCount[bitRef] = CountSetBits(mask);
+            variationCount = (int)(1LU << bitCount[bitRef]);
+            for (i=0; i<variationCount; i++)
+            {
+                occupancyVariation[bitRef][i] = 0; 
+
+                // find bits set in index "i" and map them to bits in the 64 bit "occupancyVariation"
+
+                setBitsInIndex = Bitboards.getSetBits(i); // an array of integers showing which bits are set
+                for (j=0; setBitsInIndex[j] != -1; j++)
+                {
+                    occupancyVariation[bitRef][i] |= (1LU << setBitsInMask[setBitsInIndex[j]]);
+                }
+                
+                if (isRook)
+                {
+                    for (j=bitRef+8; j<=55 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j+=8);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                    for (j=bitRef-8; j>=8 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j-=8);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                    for (j=bitRef+1; j%8!=7 && j%8!=0 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j++);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                    for (j=bitRef-1; j%8!=7 && j%8!=0 && j>=0 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j--);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                }
+                else
+                {
+                    for (j=bitRef+9; j%8!=7 && j%8!=0 && j<=55 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j+=9);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                    for (j=bitRef-9; j%8!=7 && j%8!=0 && j>=8 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j-=9);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                    for (j=bitRef+7; j%8!=7 && j%8!=0 && j<=55 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j+=7);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                    for (j=bitRef-7; j%8!=7 && j%8!=0 && j>=8 && (occupancyVariation[bitRef][i] & (1LU << j)) == 0; j-=7);
+                    if (j>=0 && j<=63) occupancyAttackSet[bitRef][i] |= (1LU << j);
+                }
+            }
+        }
+    }
+
+
+        public void generateMoveDatabase(bool isRook)
+    {
+        ulong validMoves;
+        int variations, bitCount;
+        int bitRef, i, j, magicIndex;
+    
+        for (bitRef=0; bitRef<=63; bitRef++)
+        {
+            bitCount = isRook ? CountSetBits(occupancyMaskRook[bitRef]) : CountSetBits(occupancyMaskBishop[bitRef]);
+            variations = (int)(1L << bitCount);
+            
+            for (i=0; i<variations; i++)
+            {
+                validMoves = 0;
+                if (isRook)
+                {
+                    magicIndex = (int)((occupancyVariation[bitRef][i] * magicNumberRook[bitRef]) >> magicNumberShiftsRook[bitRef]);
+
+                    for (j=bitRef+8; j<=63; j+=8) { validMoves |= (1LU << j); if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) break; }
+                    for (j=bitRef-8; j>=0; j-=8) { validMoves |= (1LU << j); if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) break; }
+                    for (j=bitRef+1; j%8!=0; j++) { validMoves |= (1LU << j); if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) break; }
+                    for (j=bitRef-1; j%8!=7 && j>=0; j--) { validMoves |= (1LU << j); if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) break; }
+                    
+                    magicMovesRook[bitRef][magicIndex] = validMoves;
+                }
+                else
+                {
+                    magicIndex = (int)((occupancyVariation[bitRef][i] * magicNumberBishop[bitRef]) >> magicNumberShiftsBishop[bitRef]);
+
+                    for (j=bitRef+9; j%8!=0 && j<=63; j+=9) { validMoves |= (1LU << j); if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) break; }
+                    for (j=bitRef-9; j%8!=7 && j>=0; j-=9) { validMoves |= (1LU << j); if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) break; }
+                    for (j=bitRef+7; j%8!=7 && j<=63; j+=7) { 
+                        validMoves |= (1LU << j); 
+                        if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) 
+                            break; 
+                    }
+                    for (j=bitRef-7; j%8!=0 && j>=0; j-=7) { 
+                        validMoves |= (1LU << j); 
+                        if ((occupancyVariation[bitRef][i] & (1LU << j)) != 0) 
+                            break; 
+                    }
+
+                    magicMovesBishop[bitRef][magicIndex] = validMoves;
+                }
+            }
+        }
+    }
 
         #endregion
 
